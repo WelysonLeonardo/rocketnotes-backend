fc5b168f8a23c94ba444be90a4c468e50575d8cd
Welyson <welyson.leonardo@casadosaber.com.br>
2024-07-24 08:59:31 -0300

    atualização geral

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require("path")
 
 module.exports = {
   development: {
@@ -6,11 +6,11 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
-    migrations: {
-      directory: path.resolve(__dirname, "src", "knex", "migrations")
-    },
     pool: {
       afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
     useNullAsDefault: true
   }
