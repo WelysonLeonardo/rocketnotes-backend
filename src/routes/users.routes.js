112fc8f3b92d782e09ae780799fa1c76fa1d6cbf
Welyson <welyson.leonardo@casadosaber.com.br>
2024-08-12 12:52:54 -0300

    ajuste geral

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,7 @@
 const {Router } = require ("express");
 
 const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 
@@ -18,6 +19,6 @@ function myMiddleware (request, response, next){
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/:id", usersController.update);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
 
 module.exports = usersRoutes;
