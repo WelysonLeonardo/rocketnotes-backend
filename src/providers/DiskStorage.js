e2ac9d3998a6730db6ca47e3e5d15d7f7b4a0575
Welyson <welyson.leonardo@casadosaber.com.br>
2024-08-12 18:10:50 -0300

    Create DiskStorage

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const uploadconfig = require("../configs/upload");
+
+
+class DiskStorage {
+    async saveFile(file) {
+        await fs.promises.rename(
+            path.resolve(uploadconfig.TMP_FOLDER, file),
+            path.resolve(uploadconfig.UPLOADS_FOLDER, file)
+        );
+
+        return file;
+    }
+
+    async deleteFile(file) {
+        const filePath = path.resolve(uploadconfig.UPLOADS_FOLDER, file);
+        try {
+            await fs.promises.stat(filePath);
+        } catch {
+            return;
+        }
+        await fs.promises.unlink(filePath);
+    }
+}
+
+module.exports = DiskStorage;
